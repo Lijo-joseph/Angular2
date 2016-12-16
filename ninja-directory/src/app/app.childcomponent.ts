e1bdfad99829f471558a762a105a44e2e906f0b0
Lijo <lijoseph10@gmail.com>
2016-12-16 14:56:24 +0530

    added UI for Movie ticket booking
    UI for Movie ticket booking

---
+++ ninja-directory/src/app/app.childcomponent.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+ selector: 'ChildComponent',
+
+  template :'<h1>Movie ticket Booking</h1>',
+  styleUrls: ['./app.childcomponent.css']
+
+})
+
+  
+
+export class ChildComponent {
+
+}
+
---
+++ ninja-directory/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //title = 'Movie ticket Booking!';
+fakeArray = new Array(10);
+names=['A','B','C','D'] ;
+ 
+
+}
